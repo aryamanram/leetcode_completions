5221bcf13c880385f1c9bd587577a56113856160
Aryaman Ramchandran <73668417+aryamanram@users.noreply.github.com>
2025-11-07 14:54:54 -0600

    Update 242_Valid_Anagram.py

--- 242_Valid_Anagram.py
+++ 242_Valid_Anagram.py
@@ -1,3 +1,16 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        # if len(s) != len(t):
+        #     return False
+
+        # s_count = {}
+        # t_count = {}
+
+        # for i in range(len(s)):
+        #     s_count[s[i]] = 1 + s_count.get(s[i], 0)
+        #     t_count[t[i]] = 1 + t_count.get(t[i], 0)
+
+        # return s_count == t_count
+
+
+        return sorted(t) == sorted(s)
