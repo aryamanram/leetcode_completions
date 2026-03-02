31351249115257e7fc8cf501ba9d49fdb0e614d8
Aryaman Ramchandran <aryamanramchandran@gmail.com>
2026-03-01 20:00:29 -0600

    70, fixed to python for 141 & 142

---
+++ 70_Climbing_Stairs.py
@@ -0,0 +1,6 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        sqrt5 = 5**0.5
+        phi = (1 + sqrt5) / 2
+        psi = (1 - sqrt5) / 2
+        return int((phi ** (n + 1) - psi ** (n + 1)) / sqrt5)
