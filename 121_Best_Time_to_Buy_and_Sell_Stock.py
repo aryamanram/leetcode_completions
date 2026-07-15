4f67b93fdc6b4fe82c80bd33aefb6cb1aa64a52f
Aryaman Ramchandran <aryamanramchandran@gmail.com>
2026-07-15 08:33:52 -0500

    Update 121_Best_Time_to_Buy_and_Sell_Stock.py
    better two pointer solution

--- 121_Best_Time_to_Buy_and_Sell_Stock.py
+++ 121_Best_Time_to_Buy_and_Sell_Stock.py
@@ -19,3 +19,21 @@ class Solution(object):
                 
 
         return maxProfit
+
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        buy, sell = 0, 1
+        maxProfit = 0
+
+        while sell < len(prices):
+            if prices[buy] < prices[sell]:
+                maxProfit = max(maxProfit, prices[sell] - prices[buy])
+            else:
+                buy = sell
+            sell += 1
+        
+        return maxProfit
